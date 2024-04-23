e3836bf738d6a6b242cf3d4c2f91d81369fcb178
tr-ggr <tristanjamestolentino56@gmail.com>
2024-04-24 01:27:39 +0800

    Clean up (Although code is still messy tho)

--- src/main/java/com/example/jdbcjavafx/HelloApplication.java
+++ src/main/java/com/example/jdbcjavafx/HelloApplication.java
@@ -14,7 +14,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 373, 593);
-        stage.setTitle("Hello!");
+        stage.setTitle("WildMarket");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
