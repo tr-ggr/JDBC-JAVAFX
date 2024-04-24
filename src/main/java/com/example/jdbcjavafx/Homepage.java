b7bf449473933b4f81dd850e9a3e0aa0ae60ce3a
tr-ggr <tristanjamestolentino56@gmail.com>
2024-04-25 00:31:53 +0800

    UI changes

--- src/main/java/com/example/jdbcjavafx/Homepage.java
+++ src/main/java/com/example/jdbcjavafx/Homepage.java
@@ -101,7 +101,6 @@ public class Homepage {
         clmnName.setCellValueFactory(
                 new PropertyValueFactory<>("name")
         );
-
         clmnSeller.setCellValueFactory(
                 new PropertyValueFactory<>("seller")
         );
